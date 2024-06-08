d410fc932b62e3045b7ed1d203c272e074fb9784
Guilherme Macedo <guilherme.macedo@vmtecnologia.io>
2024-06-07 22:20:44 -0300

    Configurando nosso projeto e introdução a roteamento no Vue

---
+++ src/views/Credencials/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Aqui é a página de credencials</h1>
+</template>
+
+<script>
+
+</script>
---
+++ src/views/Feedbacks/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Aqui é a página de feedbacks</h1>
+</template>
+
+<script>
+
+</script>
