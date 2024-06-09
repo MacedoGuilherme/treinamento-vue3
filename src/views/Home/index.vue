bd15fa9360d3cb29d0788eb3de894cc0736a6760
Guilherme Macedo <guilherme.macedo@vmtecnologia.io>
2024-06-09 15:41:54 -0300

    Criando tela inicial (Parte 1)

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a home</h1>
+  <CustomHeader
+    @create-account="handleAccountCreate"
+    @login="handleLogin"
+  />
+  <Contact/>
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker © 2021 </p>
+  </div>
 </template>
 
 <script>
-export default {
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import Contact from '@/views/Home/Contact.vue'
+import CustomHeader from '@/views/Home/CustomHeader.vue'
 
+export default {
+  components: { Contact, CustomHeader },
+  setup() {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin() {
+      console.log('login')
+    }
+
+    function handleAccountCreate() {
+      console.log('account create')
+    }
+
+    return {
+      handleLogin,
+      handleAccountCreate
+    }
+  }
 }
 </script>
