80129ebf18d8083d3fcddde7567939c7f724d3e7
Guilherme Macedo <guilherme.macedo@vmtecnologia.io>
2024-06-18 22:24:54 -0300

    Criando tela de credenciais (Parte 3)

--- src/views/Credencials/index.vue
+++ src/views/Credencials/index.vue
@@ -16,11 +16,19 @@
         Este aqui é a sua chave de api
       </p>
 
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
+
       <div
-        class="flex py-3 pl-5 pr-20 mt-2 rounded items-center bg-brand-gray w-1/2"
+        v-else
+        class="flex py-3 pl-5 pr-20 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2"
       >
         <span>{{ store.User.currentUser.apiKey }}</span>
-        <div class="flex ml-20 mr-1">
+        <div class="flex ml-20 mr-5">
           <icon
             name="copy"
             :color="brandColors.graydark"
@@ -42,8 +50,16 @@
         Coloque o script abaixo no seu site para começar a receber feedbacks
       </p>
 
+      <content-loader
+        v-if="store.Global.isLoading || state.isLoading"
+        class="rounded"
+        width="600px"
+        height="50px"
+      />
+
       <div
-       class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-2/3 overflow-x-scroll"
+        v-else
+       class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray w-full lg:w-2/3 overflow-x-scroll"
       >
         <pre>&lt;script src="https://MacedoGuilherme-feedbacker-widget.netlify.app?apikey={{ store.User.currentUser.apiKey }}"&gt;&lt;/script&gt;</pre>
       </div>
@@ -53,19 +69,26 @@
 
 <script>
 import HeaderLogged from '../../components/HeaderLogged'
+import ContentLoader from '../../components/ContentLoader'
 import Icon  from '../../components/Icon'
 import useStore from '@/hooks/useStore'
 import palette from '../../../palette'
+import { reactive } from 'vue'
 
 export default {
   components: {
+    ContentLoader,
     HeaderLogged,
     Icon
   },
   setup () {
     const store =  useStore()
+    const state = reactive({
+      isLoading: false
+    })
 
     return {
+      state,
       store,
       brandColors: palette.brand
     }
