f66467f67c7fd69dd4d732bc079cb2d7d116cfc6
Guilherme Macedo <guilherme.macedo@vmtecnologia.io>
2024-06-21 22:08:59 -0300

    Criando tela de feedbacks (Parte 4)

---
+++ src/components/FeedbackCard/Badge.vue
@@ -0,0 +1,47 @@
+<template>
+  <span
+    :class="`bg-${classColor}`"
+    class="p-2 text-xs font-black text-white uppercase rounded-full"
+  >
+    {{ label }}
+  </span>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  setup (props) {
+    const label = computed(() => {
+      if (props.type === 'ISSUE') {
+        return 'Problemas'
+      } else if (props.type === 'IDEA') {
+        return 'Ideia'
+      } else {
+        return 'Outros'
+      }
+    })
+
+    const classColor = computed(() => {
+      if (props.type === 'ISSUE') {
+        return 'brand-danger'
+      } else if (props.type === 'IDEA') {
+        return 'brand-warning'
+      } else {
+        return 'brand-graydark'
+      }
+    })
+
+    return {
+      label,
+      classColor
+    }
+  }
+}
+</script>
